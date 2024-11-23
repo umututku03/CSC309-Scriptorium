a73355b41e294939f34a6df716bf002e89dbc951
bayazitbora <bayazitbora19@gmail.com>
2024-11-23 11:23:18 -0500

    Modified seed.js to pre-populate the database with synthetic data, using the faker-js library

--- PP2/scriptorium/prisma/seed.js
+++ PP2/scriptorium/prisma/seed.js
@@ -1,9 +1,11 @@
 // prisma/seed.js
+const { PrismaClient } = require("@prisma/client");
+const { faker } = require("@faker-js/faker");
 const bcrypt = require("bcrypt");
-const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
 async function main() {
+  // Create the admin user
   const adminEmail = process.env.ADMIN_EMAIL;
   const adminPassword = process.env.ADMIN_PASSWORD;
 
@@ -25,6 +27,87 @@ async function main() {
   });
 
   console.log(`Admin user created with email: ${adminEmail}`);
+
+
+  // Create synthetic users
+  const users = [];
+  for (let i = 0; i < 10; i++) {
+    const user = await prisma.user.create({
+      data: {
+        firstName: faker.person.firstName(),
+        lastName: faker.person.lastName(),
+        email: faker.internet.email(),
+        password: faker.internet.password(8),
+        avatar: faker.image.avatar(),
+        phone: faker.phone.number(),
+        role: "USER", // First user is admin
+      },
+    });
+    users.push(user);
+  }
+  console.log("Users created.");
+
+  // Create synthetic code templates
+  const templates = [];
+  for (let i = 0; i < 20; i++) {
+    const template = await prisma.codeTemplate.create({
+      data: {
+        title: faker.lorem.sentence(),
+        explanation: faker.lorem.paragraph(),
+        tags: faker.lorem.words(3),
+        code: `// Example code: ${faker.hacker.phrase()}`,
+        language: faker.helpers.arrayElement([
+          "Python",
+          "JavaScript",
+          "Java",
+          "C++",
+          "C",
+        ]),
+        userId: users[faker.number.int({ min: 0, max: users.length - 1 })].id,
+      },
+    });
+    templates.push(template);
+  }
+  console.log("Code templates created.");
+
+  // Create synthetic blog posts
+  const blogs = [];
+  for (let i = 0; i < 15; i++) {
+    const blog = await prisma.blogPost.create({
+      data: {
+        title: faker.lorem.sentence(),
+        description: faker.lorem.sentence(),
+        tag: faker.lorem.word(),
+        content: faker.lorem.paragraphs(3),
+        userId: users[faker.number.int({ min: 0, max: users.length - 1 })].id,
+        templates: {
+          connect: [
+            {
+              id: templates[
+                faker.number.int({ min: 0, max: templates.length - 1 })
+              ].id,
+            },
+          ],
+        },
+      },
+    });
+    blogs.push(blog);
+  }
+  console.log("Blog posts created.");
+
+  // Create synthetic comments
+  for (let i = 0; i < 50; i++) {
+    await prisma.comment.create({
+      data: {
+        content: faker.lorem.sentence(),
+        userId: users[faker.number.int({ min: 0, max: users.length - 1 })].id,
+        blogPostId: blogs[faker.number.int({ min: 0, max: blogs.length - 1 })].id,
+      },
+    });
+  }
+  console.log("Comments created.");
+
+  console.log("Database seeded successfully!");
 }
 
 main()
