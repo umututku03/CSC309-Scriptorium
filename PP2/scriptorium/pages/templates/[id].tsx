bf0c626247226134ec1632eafc347a715ddd295d
jihyuky <79386734+jihyuky@users.noreply.github.com>
2024-11-27 10:37:11 -0500

    blogposts to code templates

--- PP2/scriptorium/pages/templates/[id].tsx
+++ PP2/scriptorium/pages/templates/[id].tsx
@@ -35,8 +35,7 @@ export default function TemplateDetails() {
       fetchUser(accessToken);
     }
   }, [id]);
-  console.log(template);
-  console.log("HELLO ");
+
   const fetchTemplate = async () => {
     try {
       const response = await axios.get(`/api/codetemplates/${id}`);
@@ -216,25 +215,28 @@ export default function TemplateDetails() {
               </>
             )}
           </div>
-          {/* Blog Posts Section */}
-          <div>
-            <h2 className="text-xl ">Relevant Blogposts:</h2>
-            <ul className="list-disc list-inside marker:text-sm marker:text-blue-500">
-              {template.blogPosts.map((post) => (
-                <li
-                  key={post.id}
-                  className="marker:text-sm marker:text-primary ml-3"
-                >
-                  <a
-                    href={`/blog/${post.id}`}
-                    className=" hover:underline ml-2 mr-2"
-                  >
-                    {post.title}
-                  </a>
-                </li>
-              ))}
-            </ul>
-          </div>
+          {template.blogPosts && (
+            <>
+              <div>
+                <h2 className="text-xl ">Relevant Blogposts:</h2>
+                <ul className="list-disc list-inside marker:text-sm marker:text-blue-500">
+                  {template.blogPosts.map((post) => (
+                    <li
+                      key={post.id}
+                      className="marker:text-sm marker:text-primary ml-3"
+                    >
+                      <a
+                        href={`/blog/${post.id}`}
+                        className=" hover:underline ml-2 mr-2"
+                      >
+                        {post.title}
+                      </a>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </>
+          )}
         </div>
       )}
     </div>
